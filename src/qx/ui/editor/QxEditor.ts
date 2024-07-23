0802227a8e8d1ff785ae892e27c93dbb3827a88c
peter fisk <peter.fisk@gmail.com>
2024-07-23 16:56:55 -0400

    qxeditor

--- src/qx/ui/editor/QxEditor.ts
+++ src/qx/ui/editor/QxEditor.ts
@@ -1,20 +1,42 @@
+import { QxElement } from '../../html/QxElement';
 import { QxComposite } from '../container/QxComposite';
 
 export class QxEditor extends QxComposite {
+    editor: any = null;
+    initValue:string ='';
 
     clear() {
         console.log('QxEditor clear');
     }
 
-    getValue():string {
-return '';
+    defaultEnableOnAppear(): boolean {
+        return true;
+    }
+
+    getValue(): string {
+        return '';
     }
 
     onAppear() {
+        const elem: QxElement = this.getContentElement();
+        const domNode = elem.widget._domNode;
+        const mode = (window as any).qx.lang.String.format('ace/mode/%1', ['python']);
+        const config = { mode: mode, };
+        const ace: any = (window as any).ace;
+        this.editor = ace.edit(domNode, config);
+        this.editor.setTheme('ace/theme/dreamweaver');
+        this.editor.setValue(this.initValue);
+        this.initValue = '';
     }
 
-    setValue(text:string) {
-
+    setValue(text: string) {
+        if (!this.editor){
+            this.initValue = text;
+            return;
+        }
+        this.editor.setValue(text);
+        console.log('SET VALUE');
+        (window as any).X = [this, text];
     }
 
 }
