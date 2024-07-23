5490abcfe8c4e0493ccc33a4d1c74ecbefeecc3e
peter fisk <peter.fisk@gmail.com>
2024-07-23 12:29:47 -0400

    qxlist on change

--- src/qx/ui/list/QxList.ts
+++ src/qx/ui/list/QxList.ts
@@ -5,6 +5,11 @@ export class QxList extends QxWidget {
 
     constructor() {
         super(QxFactory.list());
+        this.widget.getSelection().addListener('change', this.onChange, this);
+    }
+
+    onChange() {
+        console.log('onChange');
     }
 
     setData(labels: string[]) {
