d194e97ad55aff2457377a95fd0bd8f4fa176180
peter fisk <peter.fisk@gmail.com>
2024-08-14 14:04:16 -0400

    version

--- src/constants/ServerConstants.ts
+++ src/constants/ServerConstants.ts
@@ -3,6 +3,8 @@ export class ServerConstants {
     static MethodGet: string = 'GET';
     static MethodPost: string = 'POST';
     static MethodPut: string = 'PUT';
+    static ServiceLogin: string = 'login';
     static ServiceProjects: string = 'projects';
+    static ServiceRegister: string = 'register';
     static ServiceUsers: string = 'users';
 }
--- src/constants/Version.ts
+++ src/constants/Version.ts
@@ -1,4 +1,4 @@
 export class Version {
-    static timestamp: string = '2024-08-14 17:54';
+    static timestamp: string = '2024-08-14 18:00';
     static version: string = '1.0.3';
 }
