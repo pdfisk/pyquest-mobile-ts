dfa632bc3e4a259b380503541cd1687d60ccdd3d
peter fisk <peter.fisk@gmail.com>
2024-09-22 16:13:13 -0400

    editor page

--- src/ui/pages/EditorPage.ts
+++ src/ui/pages/EditorPage.ts
@@ -2,6 +2,7 @@ import { LabelConstants } from "../../constants/LabelConstants";
 import { AbstractPage } from "./AbstractPage";
 
 export class EditorPage extends AbstractPage {
+    ace: any;
     editor: any;
     initValue: string = '';
     static instance: EditorPage;
@@ -13,11 +14,12 @@ export class EditorPage extends AbstractPage {
     }
 
     static setCode(code: string) {
-        this.getInstance().setCode(code);
+        this.getInstance().setValue(code);
     }
 
     private constructor() {
         super();
+        this.ace = (window as any).ace;
         this.setTitle(LabelConstants.PageEditor);
     }
 
@@ -26,17 +28,29 @@ export class EditorPage extends AbstractPage {
         const cfg: any = { mode: 'ace/mode/python' };
         const content = this.widget.getContent();
         content._setStyle('height', '100%');
-        const ace = (window as any).ace;
-        this.editor = ace.edit(content.getContentElement(), cfg);
-        this.setCode(this.initValue);
+        this.editor = this.ace.edit(content.getContentElement(), cfg);
+        this.setValue(this.initValue);
         (window as any).X = this;
     }
 
-    setCode(code: string) {
+    setLine(line: number) {
         if (this.editor)
-            this.editor.setValue(code);
+            this.editor.moveCursorTo(line - 1, 0);
+    }
+
+    setRange(startRow: number, startCol: number, endRow: number, endCol: number) {
+        if (this.editor)
+            this.editor.selection.setRange(new this.ace.Range(startRow, startCol, endRow, endCol));
+    }
+
+    setValue(value: string) {
+        if (this.editor) {
+            this.editor.setValue(value);
+            this.setLine(0);
+        }
         else
-            this.initValue = code;
+            this.initValue = value;
+        this.setRange(0, 0, 0, 0);
     }
 
 }
