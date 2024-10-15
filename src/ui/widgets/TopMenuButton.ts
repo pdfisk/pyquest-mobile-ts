2f2e11d57f9d3b31d2c05901cdcc7a642acc6b2b
peter fisk <peter.fisk@gmail.com>
2024-10-15 15:24:04 -0400

    top menu button

--- src/ui/widgets/TopMenuButton.ts
+++ src/ui/widgets/TopMenuButton.ts
@@ -2,13 +2,12 @@ import { QxMobileApplication } from "../../qx/application/QxMobileApplication";
 import { LabelConstants } from "../../constants";
 import { QxNavigationBarButton } from "../../qx/ui/mobile/navigationBar/QxNavigationBarButton";
 import { AbstractRoutingPage } from "../pages/AbstractRoutingPage";
+import { PageConstants } from "../../constants/PageConstants";
 
 export class TopMenuButton extends QxNavigationBarButton {
 
     constructor() {
-        const fn = () => {
-            QxMobileApplication.executeGet('/');
-             };
+        const fn = () => { QxMobileApplication.executeGet(PageConstants.routeOverview); };
         super(LabelConstants.ButtonLabelTopMenu, fn);
     }
 
