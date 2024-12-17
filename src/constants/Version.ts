bb34ffb3bc6612a375cff822401a1637c0e20c4c
peter fisk <peter.fisk@gmail.com>
2024-12-17 11:19:27 -0500

    version

--- src/constants/Version.ts
+++ src/constants/Version.ts
@@ -1,4 +1,4 @@
 export class Version {
-    static timestamp: string = '2024-12-17 15:30';
+    static timestamp: string = '2024-12-17 16:15';
     static version: string = '1.0.3';
 }
--- src/qx/ui/embed/QxIframe.ts
+++ src/qx/ui/embed/QxIframe.ts
@@ -1,4 +1,4 @@
-import { ActionConstants, EventConstants, QxConstants, UrlConstants } from "../../../constants";
+import { ActionConstants, EventConstants, QxConstants } from "../../../constants";
 import { IHandleMessage } from "../../../interfaces/IHandleMessage";
 import { IframeManager } from "../../../util/IframeManager";
 import { QxFactory } from "../../factory";
@@ -6,7 +6,7 @@ import { QxWidget } from "../mobile/core/QxWidget";
 
 export class QxIframe extends QxWidget {
     deferredMessage: any = null;
-    hasLoaded:boolean = false;
+    hasLoaded: boolean = false;
     iframeDocument: any = null;
     iframeWindow: any = null;
     messageHandler: IHandleMessage;
@@ -21,23 +21,27 @@ export class QxIframe extends QxWidget {
         IframeManager.subscribe(this);
     }
 
-    addJavascript(name:string) {
+    addFunction(fnName:string, action:string) {
+        const data = { name: this.name, action: action, args: null };
+        this.iframeWindow[fnName] = function (args: any) {
+            data.args = args;
+            parent.postMessage(data);
+        };
+    }
+
+    addJavascript(name: string) {
         const script = this.iframeDocument.createElement('script');
         script.type = 'text/javascript';
         this.iframeDocument.head.appendChild(script);
         script.src = `./iframe/js/${name}.min.js`;
     }
 
-    addStylesheet(name:string) {
+    addStylesheet(name: string) {
         const link = this.iframeDocument.createElement('link');
         link.type = 'text/css';
         link.rel = 'stylesheet';
         this.iframeDocument.head.appendChild(link);
         link.href = `./iframe/css/${name}.min.css`;
-    }
-
-    getHtml(): string {
-        return 'HTML';
     }
 
     onLoad() {
@@ -48,11 +52,7 @@ export class QxIframe extends QxWidget {
         this.iframeWindow = this.widget.getWindow();
         this.iframeDocument = this.widget.getDocument();
         this.iframeDocument.title = this.name;
-        const data = { name: this.name, action: ActionConstants.ActionShowPage, args: null };
-        this.iframeWindow.showPage = function (args: any) {
-            data.args = args;
-            parent.postMessage(data);
-        };
+        this.addFunction('showPage', ActionConstants.ActionShowPage);
     }
 
     recieveMessage(message: any) {
