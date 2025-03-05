39ac5fac8fcdedb71ce2594060436339f220b1fa
peter fisk <peter.fisk@gmail.com>
2025-03-05 11:06:00 -0500

    version

--- src/constants/MobileVersion.ts
+++ src/constants/MobileVersion.ts
@@ -1,4 +1,4 @@
 export class MobileVersion {
-    static timestamp: string = '2025-03-05T06:26:05Z';
-    static version: string = '1.0.19';
+    static timestamp: string = '2025-03-05T16:02:11Z';
+    static version: string = '1.0.20';
 }
