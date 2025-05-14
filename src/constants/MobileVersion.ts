6460e69212b43408644fcb09561b2f752bb574bf
Your Name <you@example.com>
2025-05-14 14:26:22 -0400

    compile

--- src/constants/MobileVersion.ts
+++ src/constants/MobileVersion.ts
@@ -1,4 +1,4 @@
 export class MobileVersion {
-    static timestamp: string = '2025-03-27T15:05:59Z';
-    static version: string = '1.0.27';
+    static timestamp: string = '2025-05-14T18:17:46Z';
+    static version: string = '1.0.28';
 }
